b1359e51bfb32e8a3022e980f5adf9de86dca0c8
Alvoli <alvolioficial@gmail.com>
2022-07-06 01:03:45 -0400

    Primeira atualizaçao do projeto

---
+++ Dietinha.py
@@ -0,0 +1,48 @@
+import time
+Prot = 0.15
+Carb = 0.55
+Fat = 0.3
+A = int(input("Insira sua altura em centímetros:"))
+I = int(input("Insira sua idade:"))
+P = float(input("Insira o seu peso:"))
+TMB=()
+Gen =""
+def DefineGenero():
+    global Gen
+    global TMB
+    Gen =input("Insira seu gênero biológico(Homem ou Mulher):")
+    if Gen == 'H' or Gen =='h' or Gen == "Homem" or Gen == "homem":
+        TMB= (66+(13.7*P)+(5*A)-(6.8*I))
+    elif Gen =='M' or Gen == 'm' or Gen == "Mulher" or Gen == "mulher":
+        TMB=(655+(9.6*P)+(1.8*A)-(4.7*I))
+    else:
+        print("Insira uma resposta válida.")
+        DefineGenero()
+DefineGenero()
+GCD= int(input("Insira seus gastos calóricos diários(Dentre cardio, musculação e jornada de trabalho):"))
+GCD_F= GCD + TMB
+Calc_Carb= GCD_F*Carb/4
+Calc_Prot= GCD_F*Prot/4
+Calc_Fat= GCD_F*Fat/9
+Calc_Carb_Baixo= ((TMB-(TMB*0.2)+GCD)*0.55)/4
+Calc_Carb_Alto= ((TMB+(TMB*0.2)+GCD)*0.55)/4
+print(f"Seu Carboidrato diário base é de: {Calc_Carb:.2f} gramas.")
+print(f"Sua Proteína diária é de: {Calc_Prot:.2f} gramas.")
+print(f"Sua Gordura diária é de: {Calc_Fat:.2f} gramas.")
+print(f"Seu Carboidrato diário alto é de: {Calc_Carb_Alto:.2f} gramas.")
+print(f"Seu Carboidrato diário baixo é de: {Calc_Carb_Baixo:.2f} gramas.")
+input()
+
+
+'''
+A cada 100g de arroz existem 28g de carbo
+Para consumir 322g de carbo seria necessário...
+
+28/100=322/x
+      =
+28x=32200
+x=1150g
+    ou
+    1.15kg
+xd
+'''
---
+++ README.md
@@ -0,0 +1,27 @@
+# Projeto de cálculo de macronutrientes diários em uma dieta
+
+
+
+
+
+## Este projeto NÃO tem por finalidade substituir acompanhamento profissional! Siga as orientações de um nutricionista ou profissional capacitado.
+
+
+
+'Sup, guys ? ;-)
+	Pensei em desenvolver esse pequeno projeto para treinar e desenvolver melhor minhas capacidades de programação e, quem sabe, incluí-lo em meu portfólio.
+
+​	Peguei como base uma dieta com foco em hipertrofia(logo, recomendada para praticantes de musculação) que segue a seguinte regra: 55% das calorias serão consumidas em forma de carboidratos, 30% em forma de gordura e 15% em forma de proteína. (Valores tirados do curso Shape Dos Sonhos de Renato Cariani, Tati Cariani e Júlio Balestrin).
+
+Nesse script simples também incluí valores para Carboidrato alto (20% a mais) e Carboidrato Baixo (20% a menos).
+
+#### Primeiro commit
+
+Funções:
+
+- Cálculo de Macronutrientes com base na dieta Cycle Carb
+- Cálculos para diferentes gêneros biológicos
+- Cálculo de Taxa de Metabolismo Basal (Fórmula utilizada: Harris Benedict)
+
+
+
